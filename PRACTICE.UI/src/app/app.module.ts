42ca87c6627be3d46c8331131649242f2422e228
sanjay225 <stgorasiya@gmail.com>
2023-08-29 15:19:42 +0530

    Table created in category-list-component.html

--- PRACTICE.UI/src/app/app.module.ts
+++ PRACTICE.UI/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from'@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
