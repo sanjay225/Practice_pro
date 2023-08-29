2a1d2accf0a47d67390c5f50ff0592fec0b17565
sanjay225 <stgorasiya@gmail.com>
2023-08-29 12:05:41 +0530

    New Project CodePulse.API

---
+++ CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        public ApplicationDbContext DbContext { get; }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request) 
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await DbContext.Categories.AddAsync(category);
+            await DbContext.SaveChangesAsync();
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+               
+            };
+
+            return Ok(response);
+        }
+    }
+}
