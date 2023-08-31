478392d468b91295892e372c52b1f26334a38d5f
sanjay225 <stgorasiya@gmail.com>
2023-08-31 20:33:59 +0530

    cross origin change

--- CodePulse.API/CodePulse.API/Program.cs
+++ CodePulse.API/CodePulse.API/Program.cs
@@ -26,7 +26,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+ 
 app.UseAuthorization();
 
 app.MapControllers();
